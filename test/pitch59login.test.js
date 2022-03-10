feed9c865f68765b8cfbb7f6d0a88c9d5442e884
Dorian1204 <lap12005@byui.edu>
2022-03-10 11:08:26 -0700

    clone

---
+++ test/pitch59login.test.js
@@ -0,0 +1,35 @@
+import assert from 'assert'; //adding assert to the login test
+import fetch from 'node-fetch';
+import config from 'config';
+
+const loginUrl = config.get("pitch59-url") + "/api/account/login";
+
+it(`Testing to see if we can signin`, async () => {
+  let options = {
+    method: "POST",
+    headers: {
+      "content-type": "application/json"
+    },
+    body: JSON.stringify({
+      emailId: "p59testa@gmail.com",
+      password: "P@ssw0rd"
+    })
+  };
+
+  let errorWasCaught = false;
+  let errorCaught = null;
+  let json = null;
+  try {
+    const response = await fetch(loginUrl, options);
+    json = await response.json();
+    console.log("Response", json);
+  } catch (exception) {
+    errorCaught = exception;
+    errorWasCaught = true;
+    throw console.log(errorCaught);
+  }
+
+  assert.equal(errorWasCaught,false);
+  console.log(json.code);
+  assert.equal(json.code,2012);
+});
