16f0bf8d3cf06c82f5f6a994f41293786dbd178a
whitewater123 <derekwashburn@hotmail.com>
2022-03-24 11:07:52 -0600

    changed

--- test/loadderek.test.js
+++ test/loadderek.test.js
@@ -1,8 +1,7 @@
 import loadderekurl from '../utils/loadderekurl.mjs';
 import assert from 'assert';
-import { url } from 'inspector';
 
-IDBTransaction("Tests Derek URL Endpoint", async ()=>{
+it("Tests Derek URL Endpoint", async ()=>{
     const urlResponse = await loadderekurl();
     console.log("URL Reponse: ")+urlResponse;
     assert.equal(urlResponse, "Hello Derek");
