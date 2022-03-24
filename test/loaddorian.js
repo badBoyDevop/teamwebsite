5db9270820be8deed3de4a0c355dae7ece488a6f
Dorian1204 <lap12005@byui.edu>
2022-03-24 16:32:20 -0600

    test enviorment

---
+++ test/loaddorian.js
@@ -0,0 +1,16 @@
+import fetch from "node-fetch";
+
+const loaddorian = async () => {
+
+    const options = {
+        method: 'GET',
+        headers: {'Content-Type':'application/json'}
+    };
+
+    const apiResponse = await fetch('http://34.71.252.196/dorian',options);
+    const testResponse = await apiResponse.text();
+
+    return testResponse;
+}
+
+export default loaddorian;
---
+++ utils/loaderdorian.mjs
@@ -0,0 +1,11 @@
+import assert from 'assert';
+import loaddorian from '../utils/loaderdorian.mjs';
+
+it("Test Dorian URL Endpoint", async()=>{ 
+
+    const urlResponse = await loaddorian();
+    console.log("URL Response: ")+urlResponse;
+ 
+    assert.equal(urlResponse, "Hello Dorian!");
+ } 
+ );
