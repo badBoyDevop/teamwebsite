7468209a0f8cb79bcc4c38c6654bca851b447e1a
Dorian1204 <lap12005@byui.edu>
2022-03-29 10:08:52 -0600

    rws

--- test/loaddorian.test.js
+++ test/loaddorian.test.js
@@ -2,10 +2,8 @@ import assert from 'assert';
 import loaddorian from '../utils/loaderdorian.mjs';
 
 it("Test Dorian URL Endpoint", async()=>{ 
-
     const urlResponse = await loaddorian();
     console.log("URL Response: ")+urlResponse;
- 
     assert.equal(urlResponse, "Hello Dorian!");
  } 
  );
